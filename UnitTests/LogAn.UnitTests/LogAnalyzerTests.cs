c0446a4a509f2a3fb14c64651be14ddc74770f15
Jacinto Dominguez <jacinto@kaiamcorp.com>
2017-08-09 15:41:11 -0700

    Chapter 2 Setup

---
+++ UnitTests/LogAn.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void IsValidLogFileName_BadExtension_ReturnsFalse()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            bool result = analyzer.IsValidLogFileName("filewithbadextension.foo");
+
+            Assert.False(result);
+        }
+
+        [Test]
+        public void IsValidLogFileName_GoodExtensionLowercase_ReturnsTrue()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            bool result = analyzer.IsValidLogFileName("filewithgoodextension.slf");
+
+            Assert.True(result);
+        }
+
+        [Test]
+        public void IsValidLogFileName_GoodExtensionUppercase_ReturnsTrue()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            bool result = analyzer.IsValidLogFileName("filewithgoodextension.SLF");
+
+            Assert.True(result);
+        }
+    }
+}
